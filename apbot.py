809c122f1ee1b00b82a555efa2f77a4e4956e634
Qxe5 <NULL>
2021-07-08 21:30:57 +0100

    add scraper

---
+++ apbot.py
@@ -0,0 +1,25 @@
+import http.client
+import json
+import time
+
+while True:
+    conn = http.client.HTTPSConnection('boards.4chan.org')
+    conn.request('GET', '/b/catalog')
+
+    resp = conn.getresponse().read()
+    resp = resp[resp.index(b'var catalog =') + 14 : resp.index(b':15};') + 4]
+    resp = json.loads(resp).get('threads')
+
+    ids = []
+    for thread in resp:
+        if 'loli' in resp.get(thread).get('teaser').lower():
+            ids.append(thread)
+
+    for id in ids:
+        conn.request('GET', '/b/thread/' + id)
+
+        with open('serve/thread/' + id + '.html', 'wb') as thread:
+            thread.write(conn.getresponse().read())
+
+    conn.close()
+    time.sleep(120)
