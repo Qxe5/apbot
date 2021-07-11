e0c3ada4b8b62163cdf1b4f2155825ee9d27cafc
Qxe5 <NULL>
2021-07-11 05:04:14 +0100

    add target /soc/

--- apbot.py
+++ apbot.py
@@ -4,34 +4,37 @@ import time
 import os.path
 
 while True:
-    conn = http.client.HTTPSConnection('boards.4chan.org')
-    conn.request('GET', '/b/catalog')
+    for board in ['/b/', '/soc/']:
+        conn = http.client.HTTPSConnection('boards.4chan.org')
+        conn.request('GET', board + 'catalog')
 
-    resp = conn.getresponse().read()
-    resp = resp[resp.index(b'var catalog =') + 14 : resp.index(b':15};') + 4]
-    resp = json.loads(resp).get('threads')
+        resp = conn.getresponse().read()
+        resp = resp[resp.index(b'{"threads"') : resp.index(b':15};') + 4]
+        resp = json.loads(resp).get('threads')
 
-    ids = []
-    for thread in resp:
-        post = resp.get(thread).get('teaser').lower()
+        ids = []
+        for thread in resp:
+            post = resp.get(thread).get('teaser').lower()
 
-        if 'loli' in post or 'shota' in post:
-            ids.append(thread)
+            keywords = ['loli', 'shota', 'wickr']
+            if any([keyword in post for keyword in keywords]):
+                ids.append(thread)
 
-    for id in ids:
-        conn.request('GET', '/b/thread/' + id)
-        resp = conn.getresponse()
+        for id in ids:
+            conn.request('GET', board + 'thread/' + id)
+            resp = conn.getresponse()
 
-        if resp.status == 200:
-            filepath = 'serve/thread/' + id + '.html'
+            if resp.status == 200:
+                filepath = 'serve/thread' + board + id + '.html'
 
-            if not os.path.exists(filepath):
-                print('Creating ' + id + ' ...')
-            else:
-                print('Updating ' + id + ' ...')
+                if not os.path.exists(filepath):
+                    print('Creating ' + id + ' ...', end = ' ')
+                else:
+                    print('Updating ' + id + ' ...', end = ' ')
+                print('[' + board + ']')
 
-            with open(filepath, 'wb') as thread:
-                thread.write(resp.read())
+                with open(filepath, 'wb') as thread:
+                    thread.write(resp.read())
 
     conn.close()
     time.sleep(120)
